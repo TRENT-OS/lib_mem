a6a0f5a2ac7933a6b7d0ea026ebea9452f59d260
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.de>
2021-01-25 14:59:00 +0100

    [TST] Add mocks target
    If other library is under test and depends on this library, it will
    have now the possibility to link the target containing mocks.
    
    Fixes:
    SEOS-2087

---
+++ mocks/include/mem_config.h
@@ -0,0 +1,11 @@
+/* Copyright (C) 2021, HENSOLDT Cyber GmbH */
+
+/**
+ * @file
+ *
+ * @brief Memory configuration
+ */
+#pragma once
+
+// Use the stdlib alloc
+#define Memory_Config_USE_STDLIB_ALLOC
