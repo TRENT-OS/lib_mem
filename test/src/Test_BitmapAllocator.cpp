dae81410eb7b980d6aea6b123287728057a81d3f
Matthias Buhl <matthias.buhl@hensoldt-cyber.de>
2021-02-05 09:15:18 +0100

    [REF] Turn 'C' macros into 'C++' constexpr

--- test/src/Test_BitmapAllocator.cpp
+++ test/src/Test_BitmapAllocator.cpp
@@ -11,9 +11,12 @@ extern "C"
 #include <limits.h>
 }
 
-#define NUM_MEMORY_ELEMENTS (((sizeof(BitmapAllocator_BitmapSlot) * CHAR_BIT) * 2) + 1)
-// our elements are uint64_t data
-#define ALLOCATOR_BUFF_SIZE (NUM_MEMORY_ELEMENTS * sizeof(uint64_t))
+// Number of elements purposely set not to be a multiple of
+// BitmapAllocator_BitmapSlot
+constexpr unsigned kNumMemoryElements = (((sizeof(BitmapAllocator_BitmapSlot) *
+                                           CHAR_BIT) * 2) + 1);
+// Test-Elements are uint64_t data
+constexpr unsigned kAllocatorBufSize = (kNumMemoryElements* sizeof(uint64_t));
 
 static BitmapAllocator bmAllocator;
 static void* baseAddr = NULL;
@@ -27,7 +30,7 @@ TEST(Test_BitmapAllocator, construction)
 {
     bool ok = BitmapAllocator_ctor(&bmAllocator,
                                    sizeof(uint64_t),
-                                   NUM_MEMORY_ELEMENTS);
+                                   kNumMemoryElements);
     ASSERT_TRUE(ok);
 }
 
@@ -42,11 +45,11 @@ allocate_full_memory_until_boundary(void)
     baseAddr = lastAddr;
 
     // this will allocate the memory by twos elements that is why the limit of
-    // the loop is set to NUM_MEMORY_ELEMENTS / 2, moreover the number of elements
+    // the loop is set to kNumMemoryElements / 2, moreover the number of elements
     // was purposely set not to be a multiple of BitmapAllocator_BitmapSlot, two
     // extra elements are added at the enBitmapAllocator_BitmapSlot, one
     // extra element are added at the end.
-    for (unsigned i = 0; i < NUM_MEMORY_ELEMENTS / 2 - 1; i++)
+    for (unsigned i = 0; i < kNumMemoryElements / 2 - 1; i++)
     {
         void* addr = BitmapAllocator_alloc(BitmapAllocator_TO_ALLOCATOR(&bmAllocator),
                                            sizeof(uint64_t) * 2);
@@ -70,7 +73,7 @@ TEST(Test_BitmapAllocator, allocate_full_memory_until_boundary)
 
 TEST(Test_BitmapAllocator, create_a_hole_and_find_back_space_there)
 {
-    void* addr = &((uint64_t*) baseAddr)[NUM_MEMORY_ELEMENTS / 2];
+    void* addr = &((uint64_t*) baseAddr)[kNumMemoryElements / 2];
     BitmapAllocator_free(BitmapAllocator_TO_ALLOCATOR(&bmAllocator), addr);
     // check that fails if require more
     void* newAddr = BitmapAllocator_alloc(BitmapAllocator_TO_ALLOCATOR(
@@ -99,13 +102,13 @@ TEST(Test_BitmapAllocator, create_a_hole_and_find_back_space_there)
 // test free all memory and then make big alloc
 TEST(Test_BitmapAllocator, free_all_and_realloc)
 {
-    for (unsigned i = 0; i < ALLOCATOR_BUFF_SIZE; i++)
+    for (unsigned i = 0; i < kAllocatorBufSize; i++)
     {
         BitmapAllocator_free(BitmapAllocator_TO_ALLOCATOR(&bmAllocator),
                              &((char*) baseAddr)[i]);
     }
     void* addr = BitmapAllocator_alloc(BitmapAllocator_TO_ALLOCATOR(&bmAllocator),
-                                       ALLOCATOR_BUFF_SIZE + 1);
+                                       kAllocatorBufSize + 1);
     ASSERT_EQ(addr, nullptr);
     allocate_full_memory_until_boundary();
 }
@@ -113,16 +116,16 @@ TEST(Test_BitmapAllocator, free_all_and_realloc)
 // test free all memory and then make big alloc
 TEST(Test_BitmapAllocator, free_all_and_realloc_in_once)
 {
-    for (unsigned i = 0; i < ALLOCATOR_BUFF_SIZE; i++)
+    for (unsigned i = 0; i < kAllocatorBufSize; i++)
     {
         BitmapAllocator_free(BitmapAllocator_TO_ALLOCATOR(&bmAllocator),
                              &((char*) baseAddr)[i]);
     }
     void* addr = BitmapAllocator_alloc(BitmapAllocator_TO_ALLOCATOR(&bmAllocator),
-                                       ALLOCATOR_BUFF_SIZE + 1);
+                                       kAllocatorBufSize + 1);
     ASSERT_EQ(addr, nullptr);
     addr = BitmapAllocator_alloc(BitmapAllocator_TO_ALLOCATOR(&bmAllocator),
-                                 ALLOCATOR_BUFF_SIZE);
+                                 kAllocatorBufSize);
     ASSERT_EQ(addr, baseAddr);
 }
 
